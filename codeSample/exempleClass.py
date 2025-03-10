f185900b113502ffe55e5ecb9791a7cd978edb9b
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2025-03-10 13:21:25 +0100

    Added tests

---
+++ codeSample/exempleClass.py
@@ -0,0 +1,13 @@
+# fichier de comparaison pour coloration syntaxique.
+
+class ExampleClass :
+    number = 0
+
+    def construct(self, number:int) -> None :
+        setNumber(number)
+
+    def setNumber(self, number:int) -> None :
+        self.number = number
+    
+    def getNumber(self) -> int :
+        return self.number
